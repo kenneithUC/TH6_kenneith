8d5ea18a5ba348b86e41152144a487ca93b8054f
kenneithUC <127652072+kenneithUC@users.noreply.github.com>
2023-04-08 23:27:44 +0700

    Add files via upload

---
+++ TH week 6 kenneith/TH week 6 kenneith/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TH_week_6_kenneith
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Label wordle,intro;
+        Button play;
+        TextBox isi;
+        public static int input;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wordle = new Label();
+            wordle.Text = "WORDLE";
+            wordle.Location = new Point(85, 80);
+            wordle.Size = new Size(200, 40);
+            wordle.Font = new Font("Arial", wordle.Font.Size * 3, FontStyle.Bold);
+            this.Controls.Add(wordle);
+
+            intro = new Label();
+            intro.Text = "Set How Much You Can Guess!";
+            intro.Location = new Point(85, 120);
+            intro.Size = new Size(300, 20);
+            intro.Font = new Font("Arial", intro.Font.Size);
+            this.Controls.Add(intro);
+
+            isi = new TextBox();
+            isi.Location = new Point(115, 140);
+            isi.Size = new Size(100, 90);
+            this.Controls.Add(isi);
+
+            play = new Button();
+            play.Text = "PLAY!";
+            play.Location = new Point(127, 165);
+            play.Size = new Size(70, 20);
+            play.Click += btn_play_click;
+            this.Controls.Add(play);
+        }
+        private void btn_play_click(object sender,EventArgs e)
+        {
+            if(!int.TryParse(isi.Text, out input))
+            {
+                MessageBox.Show("Pls fill a number", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if(Convert.ToInt32(isi.Text)<=3)
+            {
+                MessageBox.Show("Number must be greater than 3","",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                input = Convert.ToInt32(isi.Text);
+                Form2 formgame = new Form2();
+                formgame.Show();
+            }
+        }
+    }
+}
---
+++ TH week 6 kenneith/TH week 6 kenneith/Form2.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TH_week_6_kenneith
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        Button[,] tempatnebak;
+        Button tempatkey,enter,delete;
+        int x, y,panjangkata,guessbrpkali,keyboardx,keyboardy,posisiguessx,posisiguessy,lebarkeyboard;
+        string[] keyboard;
+        string kata = "";
+        List<string>wordleword=new List<string>();
+        string answer;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            x = 10;
+            y = 10;
+            int totalangka= 26;
+            panjangkata = 5;
+            guessbrpkali = Form1.input;
+            tempatnebak = new Button[panjangkata, guessbrpkali];
+            keyboard= new string[26] {"Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"};
+            posisiguessx = 0;
+            posisiguessy = 0;
+            for(int i = 0; i < panjangkata; i++)
+            {
+                for (int j = 0; j <guessbrpkali; j++)
+                {
+                    tempatnebak[i, j] = new Button();
+                    tempatnebak[i, j].Tag = i.ToString() + "," + j.ToString();
+                    tempatnebak[i, j].Size = new Size(50, 50);
+                    tempatnebak[i, j].Location = new Point(x, y);
+                    this.Controls.Add(tempatnebak[i,j]);
+                    y+= 50;
+                }
+                y= 10;
+                x+= 50;
+            }
+            keyboardx = 300;
+            keyboardy = 10;
+            for(int u=0; u <totalangka; u++)
+            {
+                if(u==10)
+                {
+                    keyboardx = 330;
+                    keyboardy = 70;
+                }
+                if(u==19)
+                {
+                    keyboardx = 390;
+                    keyboardy = 130;
+                }
+                tempatkey = new Button();
+                tempatkey.Text = keyboard[u];
+                tempatkey.Size = new Size(50, 50);
+                tempatkey.Location = new Point(keyboardx, keyboardy);
+                this.Controls.Add(tempatkey);
+                tempatkey.Click += key_click;
+                keyboardx += 60;
+            }
+            delete = new Button();
+            delete.Text = "Delete";
+            delete.Size = new Size(80,50);
+            delete.Location = new Point(810, 130);
+            this.Controls.Add(delete);
+            delete.Click += delete_click;
+
+            enter=new Button();
+            enter.Text = "Enter";
+            enter.Size = new Size(80, 50);
+            enter.Location = new Point(300, 130);
+            this.Controls.Add(enter);
+            enter.Click += enter_click;
+
+            string[] wordlines = File.ReadAllLines("Wordle Word List.txt");
+            foreach (string word in wordlines)
+            {
+                wordleword.AddRange(word.Split(','));
+            }
+            answer = wordleword[new Random().Next(0, wordleword.Count - 1)].ToUpper();
+            MessageBox.Show(answer);
+        }
+
+        private void key_click(object sender,EventArgs e)
+        {
+            var key = sender as Button;
+            if (posisiguessx < 5)
+            {
+                tempatnebak[posisiguessx, posisiguessy].Text = key.Text;
+                posisiguessx++;
+            }
+        }
+        private void delete_click(object sender,EventArgs e)
+        {
+            if(posisiguessx>0)
+            {
+                posisiguessx--;
+                tempatnebak[posisiguessx, posisiguessy].Text ="";
+            }
+        }
+        private void enter_click(object sender,EventArgs e)
+        {
+            int green=0;
+            int yellow = 0;
+            if(posisiguessx!=5)
+            {
+                MessageBox.Show("harus isi satu baris baru bisa enter", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                for (int l = 0; l < panjangkata; l++)
+                {
+                    kata += tempatnebak[l, posisiguessy].Text;
+                }
+                if (wordleword.Contains(kata.ToLower()))
+                {
+                    foreach (char kata in answer)
+                    {
+                        for (int l = 0; l < panjangkata; l++)
+                        {
+                            if (tempatnebak[l, posisiguessy].Text == kata.ToString())
+                            {
+                                green++;
+                                tempatnebak[l, posisiguessy].BackColor = Color.Green;
+                            }
+                            if (tempatnebak[l, posisiguessy].Text.Contains(kata.ToString()) && tempatnebak[l, posisiguessy].Text != answer[l].ToString())
+                            {
+                                tempatnebak[l, posisiguessy].BackColor = Color.Yellow;
+                            }
+                        }
+                    }
+                    posisiguessx = 0;
+                    posisiguessy++;
+                    kata = "";
+                    if(green==5)
+                    {
+                        MessageBox.Show("Yay kata ditemukan!!!", "", MessageBoxButtons.OK);
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Kata tidak ada dalam wordle word list", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    kata = "";
+                }
+            }
+        }
+    }
+}
